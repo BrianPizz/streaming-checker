8d0c295f046b37b9eb7ae45fb078cfc5665ef9c7
BrianPizz <bpizzimentijr@outlook.com>
2023-09-19 18:26:14 -0400

    added search results page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
-var searchInputEl = $('#search-value').val();
+var searchInputEl = $('#search-value')
 var searchBtnEl = $('#search-btn');
+var mediaQuery;
 
 function submitSearch(event) {
     event.preventDefault();
-
-    location.replace();
-    console.log('redirect works')
+    mediaQuery = searchInputEl.val().trim();
+    location.replace("search-results.html");
+    getStreamInfo(mediaQuery);
 }
+function getStreamInfo(mediaQuery){
+    
+    console.log(mediaQuery)
+};
+
 
 searchBtnEl.on('click', submitSearch);
+$('#back-btn').on('click', function(){
+    location.replace("index.html");
+
+})
